1c740ac9ace90f482ea4cb373d7a798b834600dc
Luis Fagundes <lhfagundes@gmail.com>
2018-07-17 11:52:08 -0300

    Basic comparison working for numbers and strings

--- customquery/parser.py
+++ customquery/parser.py
@@ -1,4 +1,4 @@
-import sqlparse
+from sqlparse import parse, tokens as t, sql
 from django.db.models import Q
 
 class Parser:
@@ -7,4 +7,46 @@ class Parser:
         self.model = model
 
     def parse(self, query):
-        return Q(x=1)
+        result = Q()
+        parsed = parse(query)[0]
+        tokens = [ tok for tok in parsed.tokens if not tok.ttype is t.Token.Text.Whitespace ]
+        return self.compare(tokens)
+
+    def compare(self, tokens):
+        assert len(tokens) == 3
+        (identifier, comparison, value) = tokens
+
+        key = identifier.value
+        key, cond = self.make_key(comparison, key)
+        kwargs = {}
+        
+        if value.ttype is t.Token.Literal.Number.Integer:
+            kwargs[key] = int(value.value)
+        elif isinstance(value, sql.Identifier):
+            kwargs[key] = value.get_name()
+        elif value.ttype is t.Token.Literal.String.Single:
+            v = value.value
+            if v.startswith("'") and v.endswith("'"):
+                v = v[1:-1]
+            kwargs[key] = v
+
+        result = Q(**kwargs)
+        if not cond:
+            result = ~result
+        return result
+
+    def make_key(self, op, key):
+        comp = t.Token.Operator.Comparison
+        if op.match(comp, '>'):
+            return [key + '__gt', True]
+        if op.match(comp, '>='):
+            return [key + '__gte', True]
+        if op.match(comp, '<'):
+            return [key + '__lt', True]
+        if op.match(comp, '<='):
+            return [key + '__lte', True]
+        if op.match(comp, '<>'):
+            return [key, False]
+        if op.match(comp, '!='):
+            return [key, False]        
+        return [key, True]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,9 +8,40 @@ class ParserTest(TestCase):
     def setUp(self):
         self.parser = Parser(TestModel)
 
-    def test_basic_number(self):
-        qq = self.parser.parse("number=1")
-        self.assertEquals(qq, Q(x=1))
+    def parse(self, query):
+        return self.parser.parse(query)
+
+    def test_number(self):
+        self.assertEquals(self.parse("number=1"), Q(number=1))
+
+    def test_whitespaces_are_ignored(self):
+        self.assertEquals(self.parse("number = 1"), Q(number=1))
+        self.assertEquals(self.parse("number  = 1"), Q(number=1))
+
+    def test_basic_operators(self):
+        self.assertEquals(self.parse("number > 1"), Q(number__gt=1))
+        self.assertEquals(self.parse("number >= 1"), Q(number__gte=1))
+        self.assertEquals(self.parse("number < 1"), Q(number__lt=1))
+        self.assertEquals(self.parse("number <= 1"), Q(number__lte=1))
+
+    def test_basic_operators(self):
+        self.assertEquals(self.parse("number > 1"), Q(number__gt=1))
+        self.assertEquals(self.parse("number >= 1"), Q(number__gte=1))
+        self.assertEquals(self.parse("number < 1"), Q(number__lt=1))
+        self.assertEquals(self.parse("number <= 1"), Q(number__lte=1))
+
+    def test_not(self):
+        self.assertEquals(self.parse("number <> 1"), ~Q(number=1))
+        self.assertEquals(self.parse("number != 1"), ~Q(number=1))
+
+    def test_single_string(self):
+        self.assertEquals(self.parse('char=foo'), Q(char="foo"))
+        self.assertEquals(self.parse('char="foo"'), Q(char="foo"))
+        self.assertEquals(self.parse("char='foo'"), Q(char="foo"))
+        
+    def test_string_phrase(self):
+        self.assertEquals(self.parse('char="foo bar"'), Q(char="foo bar"))
+        self.assertEquals(self.parse("char='foo bar'"), Q(char="foo bar"))
         
         
         
