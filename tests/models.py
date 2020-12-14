9a5bca480ea71c2437437af05fd26957422312b0
Ivan Katkov <ik52@nyu.edu>
2020-12-14 16:06:26 +0400

    Add basic math operations + - * /

--- tests/models.py
+++ tests/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class RelatedModel(models.Model):
     name = models.CharField(max_length=16)
+    numfield = models.FloatField(null=True)
+    numfield2 = models.FloatField(null=True)
 
 class TestModel(models.Model):
     charfield = models.CharField(max_length=16)
     numfield = models.IntegerField(null=True)
+    numfield2 = models.IntegerField(null=True)
     datefield = models.DateField()
     related = models.ForeignKey(RelatedModel, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=16)
